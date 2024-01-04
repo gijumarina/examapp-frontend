d3fd04b74787d7f0fa9c48a484d1fe68ff2487a2
gijumarina <gijuaurelia18@stud.ase.ro>
2024-01-04 17:32:59 +0200

    final pls

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,10 +23,10 @@ function NavBar() {
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Home  |  </Link>
           {type === "TEACHER" && (
             <>
-              <Link to="/newTest">New Test</Link>
+              <Link to="/newTest">New Test  |  </Link>
               <Link to="/myTests">My Tests</Link>
             </>
           )}
